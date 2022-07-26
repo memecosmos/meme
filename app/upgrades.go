39ea2c26e45b10723ee0311d339671c49fa84c07
MEME Chain <network@meme.sx>
2022-07-26 18:08:51 +0800

    Meme v2.0.2

--- app/upgrades.go
+++ app/upgrades.go
@@ -10,7 +10,7 @@ import (
 )
 
 // next upgrade name
-const upgradeName = "v1.2"
+const upgradeName = "v2"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *MEMEApp) RegisterUpgradeHandlers(cfg module.Configurator) {
